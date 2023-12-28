966b2d7d0e4e96580ce28ff8e5828d4ca7768db5
HELDER RUIZ MARCELINO <110069838+helderruiz@users.noreply.github.com>
2023-12-28 15:30:34 -0300

    Finalizado uma parte do projeto

---
+++ script.js
@@ -0,0 +1,99 @@
+const tbody = document.querySelector('tbody')
+const inputDescricao = document.querySelector('#desc')
+const inputValor = document.querySelector('#val')
+const selectTipo = document.querySelector('#tip')
+const btnIncluir = document.querySelector('#btn-incluir')
+
+const entrada = document.querySelector('.entrada')
+const saida = document.querySelector('.saida')
+const total = document.querySelector('.total')
+
+
+let items
+
+btnIncluir.onclick = () =>{
+    if (inputDescricao.value ==='' || inputValor.value ==='' ) {
+        return alert('Preencha todos os Campos 😜')
+    }
+    items.push({
+        desc: inputDescricao.value,
+        val: Math.abs(inputValor.value).toFixed(2),
+        tip: selectTipo.value,
+    })
+
+    setItensBD()
+
+    loadItens()
+
+    inputDescricao.value = ''
+    inputValor.value = ''
+
+}
+
+
+
+function deleteItem(index) {  
+    items.splice(index, 1)
+    setItensBD()
+    loadItens()
+}
+
+function insertItem(item, index) {
+    let tr = document.createElement('tr')
+
+    tr.innerHTML = `
+    <td>${item.desc}</td>
+    <td>${item.val}</td>
+    <td class="tabela-tipo">${item.tip === "Entrada"
+        ? '<i class="bx bxs-chevron-up-circle"></i>'
+        : '<i class="bx bxs-chevron-down-circle"></i>'
+    }</td>
+    <td class="tabela-acao">
+    <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
+    </td>
+    `
+    tbody.appendChild(tr)
+}
+
+function loadItens() {
+    items = getItensBD()
+    tbody.innerHTML = ''
+
+    items.forEach((item, index) => {
+        insertItem(item, index)
+    });
+
+    getTotals()
+}
+
+function getTotals() {
+    const novaEntrada = items
+    .filter((item) => item.tip ==="Entrada")
+    .map((transaction) => Number(transaction.val))
+    console.log(novaEntrada)
+
+    const novaSaida = items
+    .filter((item) => item.tip ==="Saída")
+    .map((transaction) => Number(transaction.val))
+    console.log(novaSaida)
+
+    const totalEntrada = novaEntrada
+    .reduce((acc, cur) => acc + cur, 0)
+    .toFixed(2)
+
+    const totalSaida = Math.abs(
+        novaSaida.reduce((acc, cur) => acc + cur, 0)
+    ).toFixed(2)
+
+    const totalItemsValor = (totalEntrada - totalSaida).toFixed(2)
+    console.log(totalItemsValor)
+
+    entrada.innerHTML = totalEntrada
+    saida.innerHTML = totalSaida
+    total.innerHTML = totalItemsValor
+}
+
+const getItensBD = () => JSON.parse(localStorage.getItem('db_items')) ?? []
+const setItensBD = () => localStorage.setItem('db_items', JSON.stringify(items))
+
+loadItens()
