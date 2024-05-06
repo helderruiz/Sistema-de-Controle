136336316491e6f5facdc1a38fc9c23f3f0880f6
HELDER RUIZ MARCELINO <110069838+helderruiz@users.noreply.github.com>
2024-05-06 10:59:17 -0300

    finalizando projeto

--- script.js
+++ script.js
@@ -1,106 +1,104 @@
-const tbody = document.querySelector('tbody')
-const inputDescricao = document.querySelector('#desc')
-const inputValor = document.querySelector('#val')
-const selectTipo = document.querySelector('#tip')
-const btnIncluir = document.querySelector('#btn-incluir')
+const tbody = document.querySelector("tbody");
+const inputDescricao = document.querySelector("#desc");
+const inputValor = document.querySelector("#val");
+const selectTipo = document.querySelector("#tip");
+const btnIncluir = document.querySelector("#btn-incluir");
 // const inputKm = document.querySelector('#kms')
 
-const entrada = document.querySelector('.entrada')
-const saida = document.querySelector('.saida')
-const total = document.querySelector('.total')
+const entrada = document.querySelector(".entrada");
+const saida = document.querySelector(".saida");
+const total = document.querySelector(".total");
 // const km = document.querySelector('.km')
 
-let items
+let items;
 
-btnIncluir.onclick = () =>{
-    if (inputDescricao.value ==='' || inputValor.value ==='' ) {
-        return alert('Preencha todos os Campos 😜')
-    }
-    items.push({
-        desc: inputDescricao.value,
-        val: Math.abs(inputValor.value).toFixed(2),
-        // kms: Math.abs(inputKm.value),
-        tip: selectTipo.value,
-    })
+btnIncluir.onclick = () => {
+  if (inputDescricao.value === "" || inputValor.value === "") {
+    return alert("Preencha todos os Campos 😜");
+  }
+  items.push({
+    desc: inputDescricao.value,
+    val: Math.abs(inputValor.value).toFixed(2),
+    // kms: Math.abs(inputKm.value),
+    tip: selectTipo.value,
+  });
 
-    setItensBD()
+  setItensBD();
 
-    loadItens()
+  loadItens();
 
-    inputDescricao.value = ''
-    inputValor.value = ''
-    inputKm.value = ''
+  inputDescricao.value = "";
+  inputValor.value = "";
+  inputKm.value = "";
+};
 
-
-}
-
-function deleteItem(index) {  
-    items.splice(index, 1)
-    setItensBD()
-    loadItens()
+function deleteItem(index) {
+  items.splice(index, 1);
+  setItensBD();
+  loadItens();
 }
 
 function insertItem(item, index) {
-    let tr = document.createElement('tr')
+  let tr = document.createElement("tr");
 
-    tr.innerHTML = `
+  tr.innerHTML = `
     <td class="td-desc">${item.desc}</td>
     <td>${item.val}</td>
-    <td class="tabela-tipo">${item.tip === "Entrada"
+    <td class="tabela-tipo">${
+      item.tip === "Entrada"
         ? '<i class="bx bxs-chevron-up-circle"></i>'
         : '<i class="bx bxs-chevron-down-circle"></i>'
     }</td>
     <td class="tabela-acao">
     <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
     </td>
-    `
-    tbody.appendChild(tr)
+    `;
+  tbody.appendChild(tr);
 }
 
 function loadItens() {
-    items = getItensBD()
-    tbody.innerHTML = ''
+  items = getItensBD();
+  tbody.innerHTML = "";
 
-    items.forEach((item, index) => {
-        insertItem(item, index)
-    });
+  items.forEach((item, index) => {
+    insertItem(item, index);
+  });
 
-    getTotals()
+  getTotals();
 }
 
 function getTotals() {
-    const novaEntrada = items
-    .filter((item) => item.tip ==="Entrada")
-    .map((transaction) => Number(transaction.val))
-    // console.log(novaEntrada)
+  const novaEntrada = items
+    .filter((item) => item.tip === "Entrada")
+    .map((transaction) => Number(transaction.val));
+  // console.log(novaEntrada)
 
-    const novaSaida = items
-    .filter((item) => item.tip ==="Saída")
-    .map((transaction) => Number(transaction.val))
-    // console.log(novaSaida)
+  const novaSaida = items
+    .filter((item) => item.tip === "Saída")
+    .map((transaction) => Number(transaction.val));
+  // console.log(novaSaida)
 
-    const totalEntrada = novaEntrada
+  const totalEntrada = novaEntrada
     .reduce((acc, cur) => acc + cur, 0)
-    .toFixed(2)
+    .toFixed(2);
 
-    const totalSaida = Math.abs(
-        novaSaida.reduce((acc, cur) => acc + cur, 0)
-    ).toFixed(2)
+  const totalSaida = Math.abs(
+    novaSaida.reduce((acc, cur) => acc + cur, 0)
+  ).toFixed(2);
 
-    const totalItemsValor = (totalEntrada - totalSaida).toFixed(2)
-    // console.log(totalItemsValor)
+  const totalItemsValor = (totalEntrada - totalSaida).toFixed(2);
+  // console.log(totalItemsValor)
 
-    // const totalKmRodados = items.reduce((accumulator, km) => accumulator + km["kms"], 0)
+  // const totalKmRodados = items.reduce((accumulator, km) => accumulator + km["kms"], 0)
 
-    entrada.innerHTML = totalEntrada
-    saida.innerHTML = totalSaida
-    total.innerHTML = totalItemsValor
-    // km.innerHTML = totalKmRodados
-
-    
+  entrada.innerHTML = totalEntrada;
+  saida.innerHTML = totalSaida;
+  total.innerHTML = totalItemsValor;
+  // km.innerHTML = totalKmRodados
 }
 
-const getItensBD = () => JSON.parse(localStorage.getItem('db_items')) ?? []
-const setItensBD = () => localStorage.setItem('db_items', JSON.stringify(items))
+const getItensBD = () => JSON.parse(localStorage.getItem("db_items")) ?? [];
+const setItensBD = () =>
+  localStorage.setItem("db_items", JSON.stringify(items));
 
-loadItens()
+loadItens();
